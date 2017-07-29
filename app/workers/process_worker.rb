947e5967ffb7ff554f282f51d7c2b762f607c6d0
anatoli.liotych <anatoli.liotych@gmail.com>
2017-07-29 15:05:05 +0300

    Fix integration issues

--- app/workers/process_worker.rb
+++ app/workers/process_worker.rb
@@ -1,6 +1,6 @@
 require './init.rb'
 require 'json'
-DEFAULT_HEADERS = { auth_token: ENV['TOKEN'], content_type: :json, accept: :json }
+DEFAULT_HEADERS = { 'AuthToken': ENV['TOKEN'], content_type: :json, accept: :json }
 EXPORT_URL = ENV['SERVER_URL'] + '/export'
 UPLOAD_URL = ENV['SERVER_URL'] + '/upload'
 
@@ -13,8 +13,8 @@ class ProcessWorker
     domain_name = domains.first['name']
     # do something
     ip_ranges = []
-    request_body = { domain_name => ip_ranges }
-    RestClient.post(UPLOAD_URL, request_body, DEFAULT_HEADERS)
+    request_body = { 'domains' => { domain_name => ip_ranges } }
+    RestClient.post(UPLOAD_URL, request_body.to_json, DEFAULT_HEADERS)
   rescue => e
     exception_info =  "\n" + e.message + "\n" + e.backtrace.join("\n")
     OSINTClient.logger.error "Request failed: #{exception_info}"
